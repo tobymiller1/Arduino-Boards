32e590a1b8634b75e1fecfa36220ce8bb36ca7e8
Travis CI <travis@travis-ci.org>
2017-10-02 23:08:30 +0000

    Deploy code docs to GitHub Pages Travis build: 183
    Commit: f4fbb288868b7d43be3d11736eb85c00ecca17c4

--- search/classes_2.js
+++ search/classes_2.js
@@ -1,6 +1,5 @@
 var searchData=
 [
-  ['chatter_5ftest_5fstate_5ft',['chatter_test_state_t',['../structTestMode___1_1chatter__test__state__t.html',1,'TestMode_']]],
   ['colorcomputer',['ColorComputer',['../classkaleidoscope_1_1StalkerEffect_1_1ColorComputer.html',1,'kaleidoscope::StalkerEffect']]],
   ['colormapeffect',['ColormapEffect',['../classkaleidoscope_1_1ColormapEffect.html',1,'kaleidoscope']]],
   ['combo_5ft',['combo_t',['../structkaleidoscope_1_1MagicCombo_1_1combo__t.html',1,'kaleidoscope::MagicCombo']]],
