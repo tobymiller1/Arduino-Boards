eb6c3913cfd40d4d9ab31959909e1035d9af08fe
Jesse Vincent <jesse@keyboard.io>
2017-12-22 14:13:28 -0800

    replace duplicate code with dep on an earlier rule

--- build-tools/makefiles/rules.mk
+++ build-tools/makefiles/rules.mk
@@ -32,8 +32,7 @@ check-docs:
 	doxygen $(PLUGIN_TEST_SUPPORT_DIR)/quality/etc/check-docs.conf 2> /dev/null >/dev/null
 	python $(PLUGIN_TEST_SUPPORT_DIR)/quality/doxy-coverage.py /tmp/undocced/xml
 
-check-astyle:
-	$(PLUGIN_TEST_SUPPORT_DIR)/quality/run-astyle
+check-astyle: astyle
 	$(PLUGIN_TEST_SUPPORT_DIR)/quality/astyle-check
 	
 cpplint-noisy:
